21b89afbee0f17f400befc9b3a4dac3cca9ad45e
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2025-03-11 14:34:03 +0200

    qt login aloitettu, ei valmis

--- bank-automat/mainwindow.cpp
+++ bank-automat/mainwindow.cpp
@@ -12,3 +12,28 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnLogin_clicked()
+{
+    QJsonObject jsonObj;
+    jsonObj.insert("username",ui->textUsername->text());
+    jsonObj.insert("password",ui->textPassword->text());
+
+
+    QString site_url="http://localhost:3000/login";
+    QNetworkRequest request(site_url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+
+    manager = new QNetworkAccessManager(this);
+    connect(manager, &QNetworkAccessManager::finished, this, &MainWindow::loginSlot);
+
+    reply = manager->post(request, QJsonDocument(jsonObj).toJson());
+}
+
+void MainWindow::loginSlot(QNetworkReply *reply)
+{
+    response_data=reply->readAll();
+    qDebug()<<response_data;
+}
+
